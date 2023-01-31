caef684a30f17f11c911c6b67b98a1d09c72f2d8
xf <790552193@qq.com>
2023-01-31 12:02:50 +0800

    init

---
+++ src/main/java/com/xf/evil/EvilApplication.java
@@ -0,0 +1,13 @@
+package com.xf.evil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvilApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EvilApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/xf/evil/EvilApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xf.evil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EvilApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
